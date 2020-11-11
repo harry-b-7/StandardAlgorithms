455a349e91c07c5fce5a5dfad8540b94ac484809
Harry Bell <bell.h@etoncollege.org.uk>
2020-11-11 11:02:46 +0000

    initial check-in of project

---
+++ StandardAlgorithms/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  StandardAlgorithms
+//
+//  Created by Bell, Harry (MAG) on 11/11/2020.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let a = 5
+print("The value of a is: \(a)")
+
