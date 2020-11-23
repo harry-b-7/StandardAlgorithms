7b5f26c2fdfcde387da39b39fc4417e29a55c704
Harry Bell <harrybell777@gmail.com>
2020-11-23 22:21:37 +0000

    Done first test on Binary Search (normal integer array) and finished linear search TDD.

--- StandardAlgorithms/Searching.swift
+++ StandardAlgorithms/Searching.swift
@@ -9,12 +9,35 @@ import Foundation
 
 class Searching {
     
-    func linearSearch(list: [Int], toFind: Int) -> Int? {
+    func linearSearch(list: [Int], toFind: Int) -> Int {
+        if list.count == 0 {
+            return -1
+        }
+        
         for i in 0..<list.count {
             if list[i] == toFind {
                 return i
             }
         }
-        return nil
+        return -2
+    }
+    
+    func binarySearch(list: [Int],toFind: Int) -> Int {
+        let leftMarker = 0
+        let rightMarker = list.count-1
+        
+        func recursiveFind(list: [Int],leftMarker: Int, rightMarker: Int) -> Int {
+            if list[rightMarker-(leftMarker/2)] < toFind {
+                let newLeftMarker = rightMarker-(leftMarker/2)+1
+                return recursiveFind(list: list,leftMarker: newLeftMarker,rightMarker: rightMarker)
+            } else if list[rightMarker-(leftMarker/2)] > toFind {
+                let newRightMarker = rightMarker-(leftMarker/2)-1
+                return recursiveFind(list: list,leftMarker: leftMarker,rightMarker: newRightMarker)
+            } else {
+                return rightMarker-(leftMarker/2)
+                
+            }
+        }
+        return recursiveFind(list: list, leftMarker: leftMarker,rightMarker: rightMarker)
     }
 }
--- StandardAlgorithmsTests/SearchingTest.swift
+++ StandardAlgorithmsTests/SearchingTest.swift
@@ -22,8 +22,41 @@ class SearchingTest: XCTestCase {
         
     }
     
-    func testLinearSeaarchWithEmptyArrayReturnsNil() {
+    func testLinearSearchWithEmptyArrayReturnsNegativeOne() {
         //arrange
-        
+        let searching = Searching()
+        let testData = [(data: [1],toFind: 0, expected: -2),(data: [],toFind: 4, expected: -1),(data: [],toFind: 143225326, expected: -1)]
+        //act
+        for test in testData {
+            let result = searching.linearSearch(list: test.data, toFind: test.toFind)
+            //assert
+            XCTAssertEqual(result, test.expected)
+        }
     }
+    
+    func testLinearSearchWithIntegerArrayWhichDoesNotContainDesiredIntegerReturnsNegativeTwo() {
+        //arrange
+        let searching = Searching()
+        let testData = [(data:[1,5,2,8,3,9],toFind: 12,expected: -2),(data:[5,2,6,15,92,72,25,43,9],toFind: 4,expected: -2),(data:[4],toFind: 7,expected: -2)]
+        //act
+        for test in testData {
+            let result = searching.linearSearch(list: test.data, toFind: test.toFind)
+            //assert
+            XCTAssertEqual(result, test.expected)
+        }
+    }
+    
+    func testBinarySearchWithSortedIntegerArrayReturnsIndexOfItem() {
+        //arrange
+        let searching = Searching()
+        let testData = [(data: [1,2,3,4,5,6,7,8],toFind: 2,expected: 1),(data: [2,4,6,8,10,12,14],toFind: 14,expected: 6)]
+        //act
+        for test in testData {
+            let result = searching.binarySearch(list: test.data, toFind: test.toFind)
+            XCTAssertEqual(result, test.expected)
+        }
+    }
+
 }
+    
+
